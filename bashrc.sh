30740d1d92865087c5084743585bd3c3d1fba43b
Sasha Mikheyev <sasha@homologo.us>
2013-09-16 15:54:00 +0900

    history optimization

--- bashrc.sh
+++ bashrc.sh
@@ -4,6 +4,16 @@
 eval `ssh-agent`
 alias gitadd='ssh-add ~/.ssh/id_rsa_github'
 
+####################
+# History settings #
+####################
+#append history to make it sharable across instances
+shopt -s histappend  
+#share history across login instances
+set PROMPT_COMMAND='history -a'  
+#extend history
+export HISTSIZE=10000
+
 # initialize tombo-specific PATH
 if [[ "$HOSTNAME" == *tombo* ]]
 then
